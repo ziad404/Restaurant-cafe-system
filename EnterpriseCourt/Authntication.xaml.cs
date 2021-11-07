c49d129a6e94d1342c4ee32137d60c50d48744bb
YoussefPasha <youssefbashafcis@gmail.com>
2021-11-07 22:44:27 +0200

    add screen Login

--- EnterpriseCourt/Authntication.xaml.cs
+++ EnterpriseCourt/Authntication.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace EnterpriseCourt
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Authntication.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Authntication : Window
     {
-        public MainWindow()
+        public Authntication()
         {
             InitializeComponent();
         }
